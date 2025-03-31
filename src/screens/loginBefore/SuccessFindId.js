451ea581d9e525f0bcc5fcce25c77a8ac14d1a10
Hong-min1010 <qza7367@gmail.com>
2025-03-31 22:10:40 +0900

    아이디 찾기 스타일 적용 및 복사 버튼 기능 구현

--- src/screens/loginBefore/SuccessFindId.js
+++ src/screens/loginBefore/SuccessFindId.js
@@ -1,14 +1,49 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Clipboard } from 'react-native';
+import Toast from 'react-native-toast-message'; // Toast를 사용하기 위해 추가
+import { commonStyles } from '../../constants/styles';
+import IconButton from '../../components/IconButton'; // IconButton 컴포넌트 임포트
+import { BLACK_COLOR } from '../../constants/colors';
+import { CustomButton } from '../../components/CustomButton';
 
 export const SuccessFindIdScreen = ({ route }) => {
   const { email } = route.params; // 이전 화면에서 전달된 이메일 정보
 
+  // 이메일 복사 함수
+  const handleCopyEmail = () => {
+    Clipboard.setString(email); // 이메일을 클립보드에 복사
+    Toast.show({
+      type: 'success',
+      text1: '아이디가 복사되었습니다.',
+      position: 'bottom',
+    });
+  };
+
+  const handleGoToLogin = () => {
+    console.log('로그인 화면으로 이동'); // 실제 네비게이션 로직 추가 필요
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>아이디 찾기 성공!</Text>
-      <Text style={styles.email}>회원님의 이메일은:</Text>
-      <Text style={styles.emailValue}>{email}</Text>
+    <View style={commonStyles.container}>
+      <Text style={styles.title}>회원님의 아이디는</Text>
+      <View style={styles.emailContainer}>
+        <Text style={styles.emailValue}>{email}</Text>
+        <IconButton 
+          name="content-copy" 
+          size={24} 
+          color="#007BFF" 
+          onPress={handleCopyEmail} 
+        />
+      </View>
+      <Text style={styles.subtitle}>입니다.</Text>
+
+      {/* 로그인 하러 가기 버튼 */}
+      <CustomButton 
+        title="로그인 하러 가기" 
+        onPress={handleGoToLogin} 
+        style={styles.loginButton} 
+      />
+
     </View>
   );
 };
@@ -19,19 +54,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 20,
+    backgroundColor: '#F8F8F8', // 배경색 설정
   },
   title: {
-    fontSize: 24,
+    fontSize: 16,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginTop: 10,
+    marginBottom: 10
   },
-  email: {
-    fontSize: 18,
+  emailContainer: {
+    flexDirection: 'row', // 텍스트와 아이콘을 가로로 배치
+    alignItems: 'center',
     marginBottom: 10,
   },
   emailValue: {
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#007BFF',
+    color: BLACK_COLOR,
+    marginRight: 8, // 텍스트와 아이콘 간격
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 30
   },
 });
